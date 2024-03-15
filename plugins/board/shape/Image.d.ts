1482c8bd241c23a4d9317cba906c34be204387ff
andrew54068 <andrew0424718012@yahoo.com.tw>
2024-03-15 09:34:32 +0800

    feat: adjust source type.

--- plugins/board/shape/Image.d.ts
+++ plugins/board/shape/Image.d.ts
@@ -5,8 +5,7 @@ import MiniBoard from '../miniboard/MiniBoard';
 export default class Image extends Phaser.GameObjects.Image {
     constructor(
         board: Board | MiniBoard,
-        source: HTMLImageElement | HTMLCanvasElement | HTMLImageElement[] | HTMLCanvasElement[] | Phaser.Renderer.WebGL.Wrappers.WebGLTextureWrapper,
-        width: number, height: number,
+        source: string | Phaser.Textures.Texture,
         tileX: number, tileY: number, tileZ?: number,
         frame?: string | number,
         addToBoard?: boolean
